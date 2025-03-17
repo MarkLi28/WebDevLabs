03e88098f085db7504f132f10aaea9d2dee3c9d1
MarkLi28 <markli@andrew.cmu.edu>
2025-03-17 18:12:55 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -31,8 +31,8 @@ function findTheBanana(L){
     });
 }
 
-findTheBanana(L1);
-findTheBanana(L2);
+//findTheBanana(L1);
+//findTheBanana(L2);
 
 function greetingFunc(){
     let d = new Date();
@@ -53,5 +53,66 @@ function greetingFunc(){
 
 greetingFunc();
 
+function addYear(){
+    let newYear = new Date().getFullYear();
+    document.getElementById("copyYear").innerHTML += newYear;
+}
 
+function showList(){
+    let list = document.getElementById("funList");
+    let button = document.getElementById("showButton");
+
+    list.style.display = "block";
+    button.style.display = "none";
+}
+
+$(document).ready(function(){
+    $("#readMore").click(function(){
+        $("#introShort").hide();
+        $("#readMore").hide();
+        $("#introLong").show();
+        $("#readLess").show();
+    });
+
+    $("#readLess").click(function(){
+        $("#introLong").hide();
+        $("#readLess").hide();
+        $("#introShort").show();
+        $("#readMore").show();
+    });
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    let isValid = true;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let comment = document.getElementById("comment").value;
+
+    let nameError = document.getElementById("nameError");
+    let emailError = document.getElementById("emailError");
+    let commentError = document.getElementById("commentError");
+    
+    nameError.innerText="";
+    emailError.innerText="";
+    commentError.innerText="";
+
+    if (!name.checkValidity()){
+        nameError.innerText = "Please enter your name.";
+        isValid = false;
+    }
+
+    else if (!email.checkValidity()){
+        emailError.innerText = "Please enter a valid email address.";
+        isValid = false;
+    }
+
+    else if (!comment.checkValidity()){
+        commentError.innerText = "Please enter your comment.";
+        isValid = false;
+    }   
+
+    if (!isValid){
+        event.preventDefault();
+    }
+});
 
